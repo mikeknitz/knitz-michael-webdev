a223b945bf339ff71a6af6fef396c6f9e906226c
Michael Knitz <michaelknitz@MikeKnitzMBPr.local>
2016-09-16 08:21:47 -0700

    setup

--- server.js
+++ server.js
@@ -8,6 +8,8 @@ app.use(bodyParser.urlencoded({ extended: true }));
 // configure a public directory to host static content
 app.use(express.static(__dirname + '/public'));
 
+require ("./test/app.js")(app);
+
 var ipaddress = process.env.PORT;
 var port      = process.env.PORT || 3000;
 
