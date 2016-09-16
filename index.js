b031b564347eb9e0736d329311df48187833473a
Michael Knitz <michaelknitz@MikeKnitzMBPr.local>
2016-09-15 23:19:17 -0700

    Hello World

--- index.js
+++ index.js
@@ -3,4 +3,4 @@ var http = require('http')
 http.createServer( function(request,response){
 	response.writeHead(200, {"Content-type": "text/plain"})
 	response.end("Hello World\n")
-}).listen(process.env.PORT)
+}).listen(process.env.PORT || 3000)
