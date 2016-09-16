46e8b4a5afd3e0b7bcd1ab93884c114dde596880
Michael Knitz <michaelknitz@MikeKnitzMBPr.local>
2016-09-16 00:49:10 -0700

    setting up

--- index.js
+++
@@ -1,6 +0,0 @@
-var http = require('http')
-
-http.createServer( function(request,response){
-	response.writeHead(200, {"Content-type": "text/plain"})
-	response.end("Hello World\n")
-}).listen(process.env.PORT || 3000)
