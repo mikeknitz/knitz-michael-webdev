7096e618b9bfb204eb5a464f4d663733f7b2bf0c
Mike Knitz <mikeknitz@gmail.com>
2016-10-24 05:00:27 -0700

    implemented register controller and view functionality

--- public/assignment/views/user/register.controller.client.js
+++ public/assignment/views/user/register.controller.client.js
@@ -4,8 +4,24 @@
         .module("WebAppMaker")
         .controller("RegisterController", RegisterController)
 
-    function RegisterController() {
+    function RegisterController($location, UserService) {
+
         var vm = this;
+
+        vm.register = register;
+        function register(username, password, verifyPassword) {
+            if (password != verifyPassword) {
+                vm.error = "Passwords do not match";
+            } else {
+                var newUser = {username: username, password: password};
+                vm.user = UserService.createUser(newUser);
+                if(! vm.user) {
+                    vm.error = "Username already taken";
+                }
+                $location.url("/user/" + vm.user._id)
+            }
+        }
+
     }
 
 })();
