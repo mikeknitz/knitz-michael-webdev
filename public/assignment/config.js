4e9dc5f7941e5a7513f77ff343d4115987d69f06
Mike Knitz <mikeknitz@gmail.com>
2016-10-23 22:01:28 -0700

    Added controllers and controllerAs to config.js

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -32,36 +32,54 @@
 
                 // Website pages
                 .when("/user/:uid/website", {
-                    templateUrl: "views/website/website-list.view.client.html"
+                    templateUrl: "views/website/website-list.view.client.html",
+                    controller: "WebsiteController",
+                    controllerAs: "model"
                 })
                 .when("/user/:uid/website/new", {
-                    templateUrl: "views/website/website-new.view.client.html"
+                    templateUrl: "views/website/website-new.view.client.html",
+                    controller: "WebsiteController",
+                    controllerAs: "model"
                 })
                 .when("/user/:uid/website/:wid", {
-                    templateUrl: "views/website/website-edit.view.client.html"
+                    templateUrl: "views/website/website-edit.view.client.html",
+                    controller: "WebsiteController",
+                    controllerAs: "model"
                 })
 
                 // Website --> Page pages
                 .when("/user/:uid/website/:wid/page", {
-                    templateUrl: "views/page/page-list.view.client.html"
+                    templateUrl: "views/page/page-list.view.client.html",
+                    controller: "PageController",
+                    controllerAs: "model"
                 })
                 .when("/user/:uid/website/:wid/page/new", {
-                    templateUrl: "views/page/page-new.view.client.html"
+                    templateUrl: "views/page/page-new.view.client.html",
+                    controller: "PageController",
+                    controllerAs: "model"
                 })
                 .when("/user/:uid/website/:wid/page/:pid", {
-                    templateUrl: "views/page/page-edit.view.client.html"
+                    templateUrl: "views/page/page-edit.view.client.html",
+                    controller: "PageController",
+                    controllerAs: "model"
                 })
 
 
                 // Website --> Page --> Widget pages
                 .when("/user/:uid/website/:wid/page/:pid/widget", {
-                    templateUrl: "views/widget/widget-list.view.client.html"
+                    templateUrl: "views/widget/widget-list.view.client.html",
+                    controller: "WidgetController",
+                    controllerAs: "model"
                 })
                 .when("/user/:uid/website/:wid/page/:pid/widget/new", {
-                    templateUrl: "views/widget/widget-choose.view.client.html"
+                    templateUrl: "views/widget/widget-choose.view.client.html",
+                    controller: "WidgetController",
+                    controllerAs: "model"
                 })
                 .when("/user/:uid/website/:wid/page/:pid/widget/:wgid", {
-                    templateUrl: "views/widget/widget-edit.view.client.html"
+                    templateUrl: "views/widget/widget-edit.view.client.html",
+                    controller: "WidgetController",
+                    controllerAs: "model"
                 })
 
 
