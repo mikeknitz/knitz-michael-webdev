39a6d258afda6af93767978e4909573a446d04a2
Mike Knitz <mikeknitz@gmail.com>
2016-10-23 18:20:33 -0700

    minor changes

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -14,13 +14,19 @@
                     controllerAs: "model"
                 })
                 .when("/login", {
-                    templateUrl: "views/user/login.view.client.html"
+                    templateUrl: "views/user/login.view.client.html",
+                    controller: "LoginController",
+                    controllerAs: "model"
                 })
                 .when("/register", {
-                    templateUrl: "views/user/register.view.client.html"
+                    templateUrl: "views/user/register.view.client.html",
+                    controller: "RegisterController",
+                    controllerAs: "model"
                 })
                 .when("/user/:uid", {
-                    templateUrl: "views/user/profile.view.client.html"
+                    templateUrl: "views/user/profile.view.client.html",
+                    controller: "ProfileController",
+                    controllerAs: "model"
                 })
 
 
