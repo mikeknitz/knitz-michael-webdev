054ec19c3cb8d5a308e191ad58b2092cbafed010
Mike Knitz <mikeknitz@gmail.com>
2016-10-24 03:28:00 -0700

    Updated controller bindings to routes

--- public/assignment/config.js
+++ public/assignment/config.js
@@ -33,34 +33,34 @@
                 // Website pages
                 .when("/user/:uid/website", {
                     templateUrl: "views/website/website-list.view.client.html",
-                    controller: "WebsiteController",
+                    controller: "WebsiteListController",
                     controllerAs: "model"
                 })
                 .when("/user/:uid/website/new", {
                     templateUrl: "views/website/website-new.view.client.html",
-                    controller: "WebsiteController",
+                    controller: "NewWebsiteController",
                     controllerAs: "model"
                 })
                 .when("/user/:uid/website/:wid", {
                     templateUrl: "views/website/website-edit.view.client.html",
-                    controller: "WebsiteController",
+                    controller: "EditWebsiteController",
                     controllerAs: "model"
                 })
 
                 // Website --> Page pages
                 .when("/user/:uid/website/:wid/page", {
                     templateUrl: "views/page/page-list.view.client.html",
-                    controller: "PageController",
+                    controller: "PageListController",
                     controllerAs: "model"
                 })
                 .when("/user/:uid/website/:wid/page/new", {
                     templateUrl: "views/page/page-new.view.client.html",
-                    controller: "PageController",
+                    controller: "NewPageController",
                     controllerAs: "model"
                 })
                 .when("/user/:uid/website/:wid/page/:pid", {
                     templateUrl: "views/page/page-edit.view.client.html",
-                    controller: "PageController",
+                    controller: "EditPageController",
                     controllerAs: "model"
                 })
 
@@ -68,17 +68,17 @@
                 // Website --> Page --> Widget pages
                 .when("/user/:uid/website/:wid/page/:pid/widget", {
                     templateUrl: "views/widget/widget-list.view.client.html",
-                    controller: "WidgetController",
+                    controller: "WidgetListController",
                     controllerAs: "model"
                 })
                 .when("/user/:uid/website/:wid/page/:pid/widget/new", {
                     templateUrl: "views/widget/widget-choose.view.client.html",
-                    controller: "WidgetController",
+                    controller: "NewWidgetController",
                     controllerAs: "model"
                 })
                 .when("/user/:uid/website/:wid/page/:pid/widget/:wgid", {
                     templateUrl: "views/widget/widget-edit.view.client.html",
-                    controller: "WidgetController",
+                    controller: "EditWidgetController",
                     controllerAs: "model"
                 })
 
